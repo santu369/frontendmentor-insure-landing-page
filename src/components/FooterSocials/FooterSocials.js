0f93d46ab8c97a943505855684dc562fbcf698e8
santu369 <janashivasantosh@gmail.com>
2021-07-28 21:14:17 +0530

    removed animations for frontendmentor challenge screenshot

--- src/components/FooterSocials/FooterSocials.js
+++ src/components/FooterSocials/FooterSocials.js
@@ -8,16 +8,16 @@ const FooterSocials = ({ icons }) => {
       {icons.map((icon, index) => {
         return (
           <li className="footer__socials-item" key={index}>
-            <SlideFromLeft>
-              {/* eslint-disable-next-line */}
-              <a href="#" target="_self" className="footer__socials-link">
-                <img
-                  src={icon.src}
-                  alt={icon.alt}
-                  className="footer__socials-icon"
-                />
-              </a>
-            </SlideFromLeft>
+            {/* <SlideFromLeft> */}
+            {/* eslint-disable-next-line */}
+            <a href="#" target="_self" className="footer__socials-link">
+              <img
+                src={icon.src}
+                alt={icon.alt}
+                className="footer__socials-icon"
+              />
+            </a>
+            {/* </SlideFromLeft> */}
           </li>
         );
       })}
