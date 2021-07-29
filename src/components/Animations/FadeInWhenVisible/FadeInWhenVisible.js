0431a7fdb0e8eba5776560bd92df29fc3c4c041b
santu369 <janashivasantosh@gmail.com>
2021-07-29 12:19:16 +0530

    added reduced animation in framer motion

--- src/components/Animations/FadeInWhenVisible/FadeInWhenVisible.js
+++ src/components/Animations/FadeInWhenVisible/FadeInWhenVisible.js
@@ -1,22 +1,24 @@
 import React, { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
-import { motion, useAnimation } from "framer-motion";
+import { motion, useAnimation, useReducedMotion } from "framer-motion";
 
 const FadeInWhenVisible = ({ children }) => {
   const controls = useAnimation();
   const [ref, inView] = useInView();
 
+  const shouldReduceMotion = useReducedMotion();
+
   useEffect(() => {
-    if (inView) {
+    if (inView || shouldReduceMotion) {
       controls.start("visible");
     }
-  }, [controls, inView]);
+  }, [controls, inView, shouldReduceMotion]);
 
   return (
     <motion.div
       ref={ref}
       animate={controls}
-      initial="hidden"
+      initial={shouldReduceMotion ? "visible" : "hidden"}
       transition={{ duration: 0.5 }}
       variants={{
         visible: { opacity: 1, scale: 1 },
--- src/components/Animations/SlideFromLeft/SlideFromLeft.js
+++ src/components/Animations/SlideFromLeft/SlideFromLeft.js
@@ -1,22 +1,24 @@
 import React, { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
-import { motion, useAnimation } from "framer-motion";
+import { motion, useAnimation, useReducedMotion } from "framer-motion";
 
 const SlideFromLeft = ({ children }) => {
   const controls = useAnimation();
   const [ref, inView] = useInView();
 
+  const shouldReduceMotion = useReducedMotion();
+
   useEffect(() => {
-    if (inView) {
+    if (inView || shouldReduceMotion) {
       controls.start("visible");
     }
-  }, [controls, inView]);
+  }, [controls, inView, shouldReduceMotion]);
 
   return (
     <motion.div
       ref={ref}
       animate={controls}
-      initial="hidden"
+      initial={shouldReduceMotion ? "visible" : "hidden"}
       transition={{ duration: 0.5 }}
       variants={{
         visible: { x: 0, opacity: 1 },
